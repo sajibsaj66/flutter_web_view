e0567217e8cfef256badec48c22d103f82d9a80b
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-07-27 23:21:04 +0600

    updated

---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(Uri.parse("https://thesoumiscanproductbd.com/"))
+  ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: WebViewWidget(controller: controller)
+      ),
+    );
+  }
+}
